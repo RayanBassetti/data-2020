1953a2da87d7bca9ce0dbc95b07850a3750d186a
RayanBassettiWSF <r.bassetti@me.com>
2020-01-25 10:24:19 +0100

    added distinct sql query

--- server/routes/GetAll.js
+++ server/routes/GetAll.js
@@ -7,14 +7,14 @@ module.exports = {
     options: {
         validate: {
             query: joi.object().keys({
-                limit: joi.number().integer().min(1).max(200).default(20),
+                limit: joi.number().integer().min(1).max(200).default(50),
                 offset: joi.number().integer().min(0).default(0)
             })
         },
         cors: true
     },
     handler: async (req, toolkit) => {
-        return db.select().from('history_daily').limit(req.query.limit).offset(req.query.offset)
+        return db.distinct("name").from('history_daily').limit(req.query.limit).offset(req.query.offset)
             .then(result => {
                 return toolkit.response({
                     statusCode: 200,
