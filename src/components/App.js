8fd41af52c8643fa375e9d36c96ad080cbe657c2
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2020-03-10 00:27:59 +0100

    started first css for the home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import '../css/index.css';
+
 import Home from './Home'
-import Footer from './Footer'
-import Header from './Header'
+// import Footer from './Footer'
+import NavMenu from './NavMenu';
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Home />
-        <Footer />
+    <div className="app_parent">
+        <div className="app">
+          <NavMenu />
+          <Home />
+        </div>
+        {/* <Footer /> */}
     </div>
   );
 }
