567fe5eeb14f8646f72e2238711ee9973f0569ee
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2020-03-19 23:52:34 +0100

    started to switch from react md to material UI

--- src/components/common/NavMenu.js
+++ src/components/common/NavMenu.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
-class NavMenu extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            valide: false
-        }
-    }
-
-    render() {
-        return(
-            <div className="navMain">
-                <div className="navMain_header">
-                    <img src="" alt="logo engie"></img>
-                    <h1 className="mainTitle">Dashboard Engie</h1>
-                </div>
+function NavMenu()  {
+    return(
+        <div className="navMain">
+            <div className="navMain_header">
+                <img src="" alt="logo engie"></img>
+                <h1 className="mainTitle">Dashboard Engie</h1>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default NavMenu
--- src/components/home/SingleClient.js
+++ src/components/home/SingleClient.js
@@ -2,35 +2,60 @@ import React from 'react';
 
 import ChartSingleClient from './ChartSingleClient';
 
-import {
-    Button,
-    Card,
-    CardActions,
-    CardTitle,
-    CardText
-} from 'react-md';
+import Card from '@material-ui/core/Card';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
 
 import { withRouter } from 'react-router-dom';
 
 function SingleClient(props) {
 
-    function handleId(idClient) {
+    const handleId = (idClient) => {
         props.history.push('/users/' + idClient, props.data)
     }
 
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
     const {data} = props;
     return (
-        <Card className="single_user md-block-centered">
-            <CardTitle
-            title={data.name}
-            subtitle={data.square_meters}
-            />
-            <CardActions expander>
-            <Button flat onClick={() => handleId(data.id)}>Go to profil</Button>
+        <Card className="single_user darkmode">
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                {data.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                {data.square_meters}
+                </Typography>
+            </CardContent>
+                <CardActions>
+                <Button size="small" color="primary" onClick={() => handleId(data.id)}>
+                    Aller sur le profil
+                </Button>
+                <IconButton
+                // className={clsx(classes.expand, {
+                //     [classes.expandOpen]: expanded,
+                // })}
+                onClick={handleExpandClick}
+                // aria-expanded={expanded}
+                aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
             </CardActions>
-            <CardText expandable>
-                <ChartSingleClient />
-            </CardText>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <ChartSingleClient />
+                </CardContent>
+            </Collapse>
         </Card>
     )
     
