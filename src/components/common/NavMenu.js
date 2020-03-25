25028d2a6b87533b011200b52026a850c895a493
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2020-03-26 00:45:20 +0100

    testing dark mode, switching branch

--- src/components/common/NavMenu.js
+++ src/components/common/NavMenu.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Switch from '@material-ui/core/Switch';
 
 function NavMenu()  {
+    const [darkMode, setDarkMode] = useState("light");
+    const [switcher, setSwitcher] = useState(false)
+
+    function toggleState() {
+        darkMode === "light" ? setDarkMode("black") : setDarkMode("light");
+        switcher === false ? setSwitcher(true) : setSwitcher(false)
+    }
+
     return(
-        <div className="navMain">
+        <div className={`navMain ${darkMode}`}>
             <div className="navMain_header">
                 <img src="" alt="logo engie"></img>
                 <h1 className="mainTitle">Dashboard Engie</h1>
+                {/* <button onClick={() => toggleState()}>{buttonText}</button> */}
+                <p>{darkMode}</p>
+                <Switch
+                    value={switcher}
+                    onChange={() => toggleState()}
+                    color="default"
+                    inputProps={{ 'aria-label': 'checkbox with default color' }}
+                />
             </div>
         </div>
     )
