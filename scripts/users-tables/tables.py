d761e2a506cf7ff191465551bf34832a83f7789b
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2020-04-26 15:09:54 +0200

    added alertes script

--- scripts/users-tables/tables.py
+++ scripts/users-tables/tables.py
@@ -11,6 +11,9 @@ from faker import Faker
 
 faker = Faker()
 
+def randomRange(start, finish) : 
+    return round(random.uniform(start, finish), 2)
+
 try:
     connection = psycopg2.connect(host=database_ids.host, database=database_ids.database, user=database_ids.user, password=database_ids.password)
     # declare a cursor
@@ -22,20 +25,34 @@ try:
     consProdClient = pd.read_sql(cons_prod_client, connection)
 
     consProd_array = []
-
+    alertes_array = []
     for _, row in fakeClients.iterrows() : 
         count = 0
-        while count < 29 :
-            date = faker.date_between(start_date='-'+ str(31 - count) + 'd', end_date='-'+ str(29 - count) + 'd')
+        while count < 35 : #since we want to have 5 weeks of data, we set the max count to 7 days * 5
+            date = faker.date_between(start_date='-'+ str(37 - count) + 'd', end_date='-'+ str(35 - count) + 'd')
+            fromGenToConsumer = randomRange(0, 10)
+            if fromGenToConsumer < 3 : 
+                alertes_array.append({
+                    "client_id": row["id"],
+                    "date": str(date),
+                    "title": "Production des panneaux trop faible",
+                    "priority": 2,
+                    "tags": "Marketing"
+                })
             consProd_array.append({
                 "client_id": row["id"],
-                "date": str(date)
+                "date": str(date),
+                "from_gen_to_consumer": fromGenToConsumer,
+                "from_grid_to_consumer": randomRange(10, 30)
             })
-            count = count + 1
+            count = count + 7
         
 
     with open('./json/cons_prod_clients.json', 'w') as outfile:
         json.dump(consProd_array, outfile)
+
+    with open('./json/alertes_clients.json', 'w') as outfile_2:
+        json.dump(alertes_array, outfile_2)
 
     ## transform the result into a permanent excel file
     # singleClient = pd.read_excel('single_client.xlsx', sheet_name='single')
@@ -47,4 +64,3 @@ except (Exception, psycopg2.DatabaseError) as error:
 finally:
     if connection is not None:
         connection.close()
-
--- todo.md
+++ todo.md
@@ -6,20 +6,22 @@
     - Faire une fonction qui prend en compte la satisfaction/relation, et qui dit donc quel est 
     l'humeur actuel de la personne 
 
-# script consProd/alertes : 
-- Faire une boucle sur le nombre d'utilisateurs de la table fake_clients
-- Faire une boucle pour chaque utilisateur aléatoire de 30 lignes
-- Dans cette boucle, on génére sur une période d'un mois une consommation et une production
-    - Prendre en compte le fait que le chiffre peut être à 0 une fois - plusieurs fois 
-    - Résultat en Kw/h, un chiffre après la virgule
-- On doit prendre en compte les alertes de consommation/production si la valeur est en dessous d'un certain seuil
-- On va retourner un objet JSON avec 2 clés : -consProd et alertes 
-- Ces 2 clés vont être séparées pour faire deux CSV et deux tables
+# script consProd : 
+- Faire une boucle sur les clients de la table fake_clients
+    - Faire une boucle pour chaque client, qui retourne 30 lignes (pour chaque jour d'un mois)
+    - Dans cette boucle, on génére sur une période d'un mois une consommation et une production
+        - Prendre en compte le fait que le chiffre peut être à 0 une fois - plusieurs fois 
+        - Résultat en Kw/h, un chiffre après la virgule
+    - Push dans la table cons_prod_clients les 30 lignes pour chaque client 
+    - table cons_prod_clients prend en colonne : 
+        - id_client
+        - date
+        - from_gen_to_consumer
+        - from_grid_to_consumer 
 
-- table prend en colonne : 
-    - id_client
-    - date
-    - from_gen_to_consumer
-    - from_grid_to_consumer 
+    - On doit prendre en compte les alertes de consommation/production si la valeur est en dessous d'un certain seuil
+    - On va retourner un objet JSON avec 2 clés : -consProd et alertes 
+    - Ces 2 clés vont être séparées pour faire deux CSV et deux tables
+
 
 
