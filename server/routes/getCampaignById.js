224052af8d372a6e48b5b0c3f85fecc2bb9d802b
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2020-05-03 16:26:37 +0200

    working on new route to get the newly created campaign

---
+++ server/routes/getCampaignById.js
@@ -0,0 +1,47 @@
+const joi = require('@hapi/joi');
+const db = require('../config/database')
+
+module.exports = {
+    method: 'GET',
+    path: '/campaigns/{campaign_id}',
+    options: {
+        validate: {
+            params: joi.object().keys({
+                campaign_id: joi.string()
+            })
+        }
+    },
+    handler: async (req, toolkit) => {
+        return db('list_campaigns').where('campaign_id', req.params.campaign_id)
+            .then(result => {
+                return toolkit.response({
+                    statusCode: 200,
+                    errors: null,
+                    message: 'OK',
+                    meta: {
+                        query: req.query,
+                        params: req.params
+                    },
+                    data: result
+                }).code(200);
+            })
+            .catch(err => {
+                return toolkit.response({
+                    statusCode: 500,
+                    message: 'Internal Server Error',
+                    errors: [
+                        {
+                            message: 'Failed to connect to database - Needed : UUID',
+                            error: err
+                        }
+                    ],
+                    meta: {
+                        query: req.query,
+                        params: req.params
+                    },
+                    data: null
+                }).code(500);
+            });
+        
+    }
+}
