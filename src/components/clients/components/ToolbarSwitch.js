f95f9f19c87106b81b67fd196e24a6de0ce803f3
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2020-05-08 11:01:24 +0200

    - added loading option for several graphs - added new icons for the toolbar

--- src/components/clients/components/ToolbarSwitch.js
+++ src/components/clients/components/ToolbarSwitch.js
@@ -10,9 +10,15 @@ function ToolbarSwitch() {
     }
     return (
         <div>
-            <Button variant="outlined" onClick={() => toggleDisplay("List")}><img alt="list" src="https://image.flaticon.com/icons/svg/2099/2099153.svg" style={img}/></Button>
-            <Button variant="outlined" onClick={() => toggleDisplay("Data")}><img alt="dataviz" src="https://image.flaticon.com/icons/svg/2575/2575553.svg" style={img}/></Button>
-            <Button variant="outlined" onClick={() => toggleDisplay("Mosaic")}><img alt="mosaic" src="https://image.flaticon.com/icons/svg/566/566001.svg" style={img}/></Button>
+            <Button variant="outlined" onClick={() => toggleDisplay("List")}>
+                <img alt="list" src="../../../ressources/lines.png" style={img}/>
+            </Button>
+            <Button variant="outlined" onClick={() => toggleDisplay("Data")}>
+                <img alt="dataviz" src="../../../ressources/data.png" style={img}/>
+            </Button>
+            <Button variant="outlined" onClick={() => toggleDisplay("Mosaic")}>
+                <img alt="mosaic" src="../../../ressources/mosaic.png" style={img}/>
+            </Button>
         </div>
     )
 }
--- src/components/home/left/HomeMessagerie.js
+++ src/components/home/left/HomeMessagerie.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { Card, CardContent } from '@material-ui/core';
 
 import HomeTitle from '../HomeTitle'
+import MessagerieLine from './MessagerieLine'
 
 function HomeMessagerie() {
     return(
-    <Card style={{marginTop: '50px', padding: '35px'}}>
+    <Card style={{marginTop: '50px', paddingRight: '35px', paddingLeft: '35px'}}>
         <HomeTitle text="Messagerie Pro" size="12px" />
         <CardContent>
+            <MessagerieLine icon="mail" text="Lorem ipsum dolor sit amet." />
+            <MessagerieLine icon="mail" text="Lorem ipsum dolor sit amet." />
+            <MessagerieLine icon="mail" text="Lorem ipsum dolor sit amet." />
         </CardContent>
         <HomeTitle text="Messagerie Clients" size="12px" />
         <CardContent>
+            <MessagerieLine icon="mail" text="Lorem ipsum dolor sit amet." />
+            <MessagerieLine icon="mail" text="Lorem ipsum dolor sit amet." />
+            <MessagerieLine icon="mail" text="Lorem ipsum dolor sit amet." />
         </CardContent>
     </Card>
     )
